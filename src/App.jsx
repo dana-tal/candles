48f1409540ebac0bb26ebd91717b944aee0603d7
Dana Tal <dana-tal@012.net.il>
2024-11-14 17:35:53 +0200

    examples of bar chart and candlestick chart

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import BarChart from './components/BarChart';
+import CandleStick from './components/CandleStick';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +12,8 @@ function App() {
     <>
      
       <h1>Welcome to candles</h1>
-     
+       <BarChart />
+       <CandleStick />
     </>
   )
 }
